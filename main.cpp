c366d9c189b4c26ba2664de29125b373397483c0
RatRatARMY <khang0762412064@outlook.com>
2026-02-02 17:54:41 +0700

    Change `main.cpp` to use login page as start page.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <QApplication>
+#include "login.hpp"
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    LoginWND* root = new LoginWND();
+    root->show();
     return QApplication::exec();
 }
